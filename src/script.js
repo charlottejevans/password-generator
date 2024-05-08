35684ce7344945061eb6b158163ebf0b3062bd74
Charlotte Evans <charlotteevans543@gmail.com>
2024-05-08 14:14:46 +0200

    Added an if else statement in the createPassword function so users can choose whether they want to include special characters or not.

--- src/script.js
+++ src/script.js
@@ -1,4 +1,5 @@
 const passwordBox = document.getElementById('password');
+const checkBox = document.getElementById('checkBox');
 const length = 12; // Adding a default length of 12 for each password generated.
 const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
@@ -6,19 +7,34 @@ const numbers = '0123456789';
 const specialCharacters = '!@#$%^&*^()_+~`|}{[];?><,./-=';
 
 function createPassword() {
-    let password = '';
-    password += upperCase[Math.floor(Math.random() * upperCase.length)];
-    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
-    password += numbers[Math.floor(Math.random() * numbers.length)];
-    password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+    if (!checkBox.checked) {
+        let password = '';
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += numbers[Math.floor(Math.random() * numbers.length)];
+        password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
 
-    const allCharacters = upperCase + lowerCase + numbers + specialCharacters;
+        const allCharacters = upperCase + lowerCase + numbers + specialCharacters;
 
-    while (length > password.length){
-        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        while (length > password.length) {
+            password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        }
+        // Displays password input in the password box.
+        passwordBox.value = password;
+    }  else {
+        let password = '';
+        password += upperCase[Math.floor(Math.random() * upperCase.length)];
+        password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+        password += numbers[Math.floor(Math.random() * numbers.length)];
+
+        const allCharacters = upperCase + lowerCase + numbers;
+
+        while (length > password.length) {
+            password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+        }
+        // Displays password input in the password box.
+        passwordBox.value = password;
     }
- // Displays password input in the password box.
-    passwordBox.value = password;
 }
 
 
@@ -27,3 +43,5 @@ function copyPassword(){
     document.execCommand('copy');
     alert('Password copied to clipboard!');
 }
+
+console.log(checkBox.checked)
