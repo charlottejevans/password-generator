b8f463f054b94caf7608a0bca08caa19aff0e51c
Charlotte Evans <charlotteevans543@gmail.com>
2024-05-08 13:48:40 +0200

    Created function: createPassword() Declared variables for generating password.

---
+++ src/script.js
@@ -0,0 +1,22 @@
+const passwordBox = document.getElementById('password');
+const length = 12; // Adding a default length of 12 for each password generated.
+const upperCase = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const lowerCase = 'abcdefghijklmnopqrstuvwxyz';
+const numbers = '0123456789';
+const specialCharacters = '!@#$%^&*^()_+~`|}{[];?><,./-=';
+
+function createPassword() {
+    let password = '';
+    password += upperCase[Math.floor(Math.random() * upperCase.length)];
+    password += lowerCase[Math.floor(Math.random() * lowerCase.length)];
+    password += numbers[Math.floor(Math.random() * numbers.length)];
+    password += specialCharacters[Math.floor(Math.random() * specialCharacters.length)];
+
+    const allCharacters = upperCase + lowerCase + numbers + specialCharacters;
+
+    while (length > password.length){
+        password += allCharacters[Math.floor(Math.random() * allCharacters.length)];
+    }
+ // Displays password input in the password box.
+    passwordBox.value = password;
+}
